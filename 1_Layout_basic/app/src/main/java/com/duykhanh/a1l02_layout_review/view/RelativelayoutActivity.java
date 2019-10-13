91b352281bedcfbfa567a0889ac510ecc3ec9251
= <=>
2019-10-13 23:37:10 +0700

    add 2 file in 1_Layout_basic

---
+++ 1_Layout_basic/app/src/main/java/com/duykhanh/a1l02_layout_review/view/RelativelayoutActivity.java
@@ -0,0 +1,18 @@
+package com.duykhanh.a1l02_layout_review.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import com.duykhanh.a1l02_layout_review.R;
+
+public class RelativelayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relativelayout);
+
+    }
+}
